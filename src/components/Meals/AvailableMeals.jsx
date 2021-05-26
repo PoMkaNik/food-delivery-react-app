d02394115c30397973caacb3aaff0079370c579d
Roman Dubovik <ip.romandubovik@gmail.com>
2021-05-26 13:51:19 +0300

    meals fetching from test backend added

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,10 +1,61 @@
-import meals from './dummy-meals';
+import { useEffect, useState } from 'react';
 import classes from './AvailableMeals.module.css';
 
 import MealItem from './MealItem/MealItem';
 import Card from '../UI/Card';
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+
+      const response = await fetch(
+        'https://react-test-3b548-default-rtdb.europe-west1.firebasedatabase.app/meals.json',
+      );
+
+      if (!response.ok) throw new Error('Something went wrong!');
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          ...data[key],
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
